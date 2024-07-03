5a7a3107ca09a424686ef0c4305f1e19e9a95b5e
kuldiplakhatariya <55498424+kuldiplakhatariya@users.noreply.github.com>
2024-07-03 16:01:03 +0200

    Add files via upload

---
+++ Java-Script-Assesment/ass.js
@@ -0,0 +1,13 @@
+function getfunction(){
+    // var ul=document.getElementById("display-java")
+    // ul.classList.style.display = block;
+    // ul.classList.style.font_Size = "20px"
+//    document.getElementById("display-java").classList.toggle(".djava");
+      //  document.getElementById("display-java").style.display = "block";
+       //console.log("buut safsf")
+       var ul=document.getElementById("display-java")
+   
+       
+      
+      
+}
